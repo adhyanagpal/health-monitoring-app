2ee3105f8c44c14ca6e3bf67ae7582da9a8b348f
Aastha <aastha05042000@gmail.com>
2020-08-06 00:31:54 +0530

    Trial to connect DB

--- project/app.js
+++ project/app.js
@@ -29,3 +29,35 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
+
+//database connection setup
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : '35.225.233.113', //localhost, appspot, 
+    database : 'docClient',
+    user     : 'user',
+    password : 'user@123456',
+  // port : '8080',
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM patient', function (error, results, fields) {
+    if (error)
+        throw error;
+
+    results.forEach(result => {
+        console.log(result);
+    });
+});
+
+connection.end();
