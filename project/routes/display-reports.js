99cf2b63d58a65f5b4cdc1a28b1b2b37e9c6600e
Adhya <adhyanagpal@gmail.com>
2020-08-11 00:33:04 +0530

    Add upload option

--- project/routes/display-reports.js
+++ project/routes/display-reports.js
@@ -1,8 +1,40 @@
 const route=require('express').Router();
+const fs = require("fs");
+const multer = require("multer");
+const path = require("path");
 
 route.get('/test-reports',(req,res)=>{
     ///fetch reports from db
     res.render('reports');
 })
+
+const uploadDir = "../uploads";
+
+// @creates the upload destination [folder] if it doesn't exist at server boot.
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+var storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, uploadDir);
+  },
+  filename: function(req, file, callback) {
+    //callback(null, Date.now() + path.extname(file.originalname));
+    callback(null, Date.now()+"_"+file.report);
+  }
+});
+
+var upload = multer({ storage: storage });
+
+route.post("/upload-report", upload.single("report"), function(req, res) {
+  const file = req.file;
+  if (!file) {
+    res.status(400).send("No File Selected");
+  }
+  console.log("File uploaded successfully")
+  res.redirect('/test-reports')
+  //res.send(file);
+});
 
 module.exports=route
--- project/routes/entry.js
+++ project/routes/entry.js
@@ -5,6 +5,7 @@ route.post('/profile-page',(req,res)=>{
     //query to db, if user already exists, redirect to dashboard
     //else redirect to profile page
     //pass user object
+    
     const usertype=req.body.user;
     console.log("User Type is: "+usertype);
     res.redirect('/profile-page/'+usertype);
