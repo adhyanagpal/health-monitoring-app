e1a38ca2d7a608e7315a51e61f4fe3ed08068b9d
Aastha <aastha05042000@gmail.com>
2020-08-22 16:09:37 +0000

    Create caledar event

--- project/db.js
+++ project/db.js
@@ -1,4 +1,5 @@
-const Datastore = require('@google-cloud/datastore');
+// const Datastore = require('@google-cloud/datastore');
+const { Datastore } = require("@google-cloud/datastore");
 const datastore = new Datastore({
   projectId: 'summer20-sps-84',
   keyFilename: 'key.json'
--- project/routes/book-appointments.js
+++ project/routes/book-appointments.js
@@ -1,7 +1,32 @@
 const route=require('express').Router()
+const datastore=require('../db.js')
 
-route.get('/book-appointment',(req,res)=>{
-    res.render('booking-appointments')
+route.post('/book-appointment',(req,res)=>{
+    // console.log(req.body)
+    
+    var doclist=[];
+    // doclist.push(req.body.email)
+    const query = datastore.createQuery('Doctor').filter('specialisation',req.body.specialisation);
+    query.run((err,entities,info)=>{
+        
+        for(var i=0;i<entities.length;i++)
+        {
+            console.log(entities[i]);
+            const doc={
+                name: entities[i].name,
+                email: entities[i].email
+            }
+            doclist.push(doc);
+        }
+        console.log(doclist)
+        var list={
+        email:req.body.email,
+        doclist: doclist
+    }
+        res.render('booking-appointments',list)
+    });
+
+    
 })
 
 module.exports=route;
